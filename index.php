3f9b4b3dcd4642cadca29ce55127d6ff4685e382
Elias Keven <eliaskeven2@gmail.com>
2024-05-02 22:56:51 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   echo "Hello World";
   $contador = 0;
   while ($contador < 1000){
-    echo "<p>Eu quero mudar!!!!</p>";
+    echo "<p>Amo minha Kakau!!!!!</p>";
       $contador = $contador + 1;
   }
 ?>
